593ffb2088754dfa5cc29f678c2c233a226782a0
Hannah Jacoby <hjacoby@grubhub.com>
2019-07-27 14:24:52 -0600

    animating gifs

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -21,6 +21,7 @@ $(document).on("click", "button", function() {
     var animal = $(this).attr("data-animal");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=WHv6n3SpE9o5X6Ccv5Xx7KN2yp1ubIOz&limit=10";
+
 $.ajax({
     url: queryURL,
     method:"GET"
@@ -31,15 +32,38 @@ $.ajax({
     console.log(results)
     for(var i = 0; i <results.length; i++) {
     var gifDiv = $("<div>")
-    gifDiv.addClass("gif")
     var rating = results[i].rating
     var p = $("<p>")
     p.append("rating: " + rating)
     var gif = $("<img>")
-    gif.attr("src", results[i].images.original.url)
+    gif.attr("data-animate", results[i].images.original.url)
+    .attr("data-still", results[i].images.original_still.url)
+    .attr("src", results[i].images.original_still.url)
+    .attr("data-state", "still")
+    .addClass("gif")
     gifDiv.append(gif)
     gifDiv.append(p)
     $("#gifs-here").prepend(gifDiv)
+
     }
 });
+
 });
+
+// click event here
+$("#gifs-here").on("click", ".gif",function() {
+    console.log("click works")
+    var state = $(this).attr("data-state");
+    console.log (state)
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+
+})
